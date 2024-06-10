560b88037bbcafe0a7ec796531cd8ba64f9060df
toxiic-me <iamtoxiicman@gmail.com>
2024-06-10 19:57:35 +0530

    added vite sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
+
+
+import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    Pages(),
+    react(),
+    Sitemap()
+  ],
 })
